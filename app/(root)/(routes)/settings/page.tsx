cae7c922e7fe5fe9cbe0957796d93b7708ba797b
engineer-gabriel-coder <jibrinsunus88@gmail.com>
2023-12-29 18:50:44 -0800

    fixed conflict

--- app/(root)/(routes)/settings/page.tsx
+++ app/(root)/(routes)/settings/page.tsx
@@ -9,7 +9,7 @@ const SettingsPage = async () => {
       <h3 className='text-lg font-medium'>Settings</h3>
       <div className='text-muted-foreground text-sm'>
         {isPro
-          ? "You are currently on a Pro plan."
+          ? "You are currently on a Pro plans."
           : "You are currently on a free plan."}
       </div>
       <SubscriptionButton isPro={isPro} />
